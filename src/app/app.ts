e59b13c0ebde7b164e1b8f02e73142454cebebfe
Frontend <jira-frontend@radcom.ro>
2025-10-04 17:51:04 +0300

    about me section and skills component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { Home } from "./home/home";
+import { AboutMe } from "./about-me/about-me";
+import { Skills } from "./skills/skills";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home],
+  imports: [Header, Home, AboutMe, Skills],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
