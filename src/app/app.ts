f1a53f92454804ec4759b1be2659e5ea4f4bb1ce
Frontend <jira-frontend@radcom.ro>
2025-10-04 18:13:32 +0300

    projects and contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
-import { Home } from "./home/home";
-import { AboutMe } from "./about-me/about-me";
-import { Skills } from "./skills/skills";
+import { Home } from './home/home';
+import { AboutMe } from './about-me/about-me';
+import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
+import { Contact } from "./contact/contact";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home, AboutMe, Skills],
+  imports: [Header, Home, AboutMe, Skills, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
