839b45410537970a698db2ed8b672d527392d089
Frontend <jira-frontend@radcom.ro>
2025-10-04 18:22:52 +0300

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { AboutMe } from './about-me/about-me';
 import { Skills } from './skills/skills';
 import { Projects } from './projects/projects';
 import { Contact } from "./contact/contact";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home, AboutMe, Skills, Projects, Contact],
+  imports: [Header, Home, AboutMe, Skills, Projects, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
